b9105a1e3b504b085bb7fd36511cefc59eeb89b1
Paul Home <paulfornia@gmail.com>
2015-10-19 22:03:50 -0400

    better secret message

--- cipher.java
+++ cipher.java
@@ -35,8 +35,8 @@ public class cipher {
         char[] keyIn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
                      , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'
                      , 'w', 'x', 'y', 'z'};
-       
-        String a = "n fgevat";
+
+        String a = "oehpr jvyyvf jnf qrnq gur jubyr gvzr";
         
         for(int i = 0; i < 26; i++){
             char[] keyOut = rotateLeft(keyIn, i);
