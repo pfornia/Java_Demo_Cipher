188ae974a88641c661cb4e187a0c82d20d65ee01
Paul Home <paulfornia@gmail.com>
2015-10-19 21:55:12 -0400

    added loop over all 26 possible shifted keys

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Work in progress.
 
 Written by Paul Fornia October 18th, 2015
+Last Updated October 19th, 2015
 
 This program serves as a small demo of Java. The cipher.java script decrypts a string encrypted by the <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar cipher</a>.
--- cipher.java
+++ cipher.java
@@ -16,14 +16,34 @@ public class cipher {
         String strOutput = new String(charOutput);
         return strOutput;
     }
-
+    
+    public static char[] rotateLeft(char[] keyOrig, int n){
+        int N = keyOrig.length;
+        char[] keyNew = new char[N];
+        n = n%N;
+        int m = N - n; 
+        for(int i = 0; i < m; i++){
+            keyNew[i] = keyOrig[i + n];
+        }
+        for(int i = m; i < N; i++){
+            keyNew[i] = keyOrig[i - m];
+        }
+        return keyNew;
+    }
+    
     public static void main(String[] args) {
-        char keyIn[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        char keyOut[] = {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'};
-        String a = "a string";
+        char[] keyIn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
+                     , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'
+                     , 'w', 'x', 'y', 'z'};
+       
+        String a = "n fgevat";
         
-        String strDecrypt = decrypt(a, keyIn, keyOut);
-        
-        System.out.println(strDecrypt);
+        for(int i = 0; i < 26; i++){
+            char[] keyOut = rotateLeft(keyIn, i);
+            String strDecrypt = decrypt(a, keyIn, keyOut);
+            System.out.println(strDecrypt);
+        }
+
+        System.out.println("ALL DONE");
     }
 }
